a647fbae9e971d35d8b8ffd0542864ccc0d1b9da
DorinVieru <dorino.vieru@gmail.com>
2024-06-24 11:14:45 +0200

    UP: add script on main.js

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 import './css/style.css'
 import './css/stylescss.scss'
 
+
 // import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
 // import styles bundle
 import 'swiper/css/bundle';
+
+// SLIDER
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+    },
+});
